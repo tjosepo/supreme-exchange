f752d45e7747cb55f1a30dcb87af4d6d7c9be5e4
tommy-josepovic <tommy-josepovic@users.noreply.github.com>
2021-04-12 13:45:26 -0400

    Add bottom nav bar

--- src/components/listing/ListingPage.jsx
+++ src/components/listing/ListingPage.jsx
@@ -1,13 +1,14 @@
 import Header from './Header';
 import PriceHistoryChart from './PriceHistoryChart';
 import ProductListings from './ProductListings';
-
+import BottomNav from './BottomNav';
 export default function ListingPage() {
   return (
     <>
       <Header>123 Sesame Street 21324</Header>
       <PriceHistoryChart />
       <ProductListings />
+      <BottomNav />
     </>
   );
 }
