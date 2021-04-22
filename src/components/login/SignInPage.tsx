56b6968fce4f2b97f38a38d368981e54b7639c92
Jason Kim <jason.k99@hotmail.com>
2021-04-21 20:28:37 -0400

    Sign In button complete

--- src/components/login/SignInPage.tsx
+++ src/components/login/SignInPage.tsx
@@ -1,17 +1,43 @@
-import { Button, Grid } from '@material-ui/core'
+import { Button,
+    createStyles,
+    makeStyles,
+    createMuiTheme,
+    ThemeProvider,
+    Theme,
+} from '@material-ui/core';
+import { green } from '@material-ui/core/colors'
 import Header from '../shared/Header';
 import Card2 from '../shared/Card2';
 import './style/SignInPage.css';
 
+const useStyles = makeStyles((theme: Theme) => 
+    createStyles({
+        margin: {
+            margin: theme.spacing(1),
+        }
+    }),
+);
+
+const theme = createMuiTheme({
+    palette: {
+      primary: green,
+    },
+  });
 
 export default function SignInPage(){
+    const classes = useStyles();
+
     return(
         <>
             <Header>
                 Supreme Exchange
             </Header>
             <Card2>
-
+                <ThemeProvider theme={theme}>
+                    <Button variant="contained" color="primary" disableElevation fullWidth className={classes.margin}>
+                        Sign In
+                    </Button>
+                </ThemeProvider>
             </Card2>
         </>
     );
