25e1fd7539e5c3c13fa49e15f81ea7b02d0fe09b
phong1233 <phong.le0499@gmail.com>
2021-04-22 22:43:50 -0400

    clean up verification process

--- src/components/login/SignInPage.tsx
+++ src/components/login/SignInPage.tsx
@@ -5,34 +5,62 @@ import { Button,
     ThemeProvider,
     Theme,
 } from '@material-ui/core';
-import { green } from '@material-ui/core/colors'
+import { green, blue, red  } from '@material-ui/core/colors'
 import Header from '../shared/Header';
 import Card2 from '../shared/Card2';
 import './style/SignPage.css';
 import InputBox from './SignInInputs'
 import {useState} from 'react';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import Typography from '@material-ui/core/Typography';
 
 import { getImageLabel } from '../utils/TensorFlow';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
 const useStyles = makeStyles((theme: Theme) => 
     createStyles({
+        container: {
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexDirection: 'column'
+        },
+        title: {
+            marginBottom: '20px'
+        },
         signButton: {
             borderRadius: 10,
-        }
+            marginTop: 10,
+            marginBottom: 10
+        },
+        wrapper: {
+            margin: theme.spacing(1),
+            position: 'relative',
+        },
+        buttonProgress: {
+            color: green[500],
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            marginTop: -12,
+            marginLeft: -12,
+        },
     }),
 );
 
 const theme = createMuiTheme({
     palette: {
       primary: green,
+      secondary: blue,
+      error: red,
     },
   });
 
 export default function SignInPage(){
     const classes = useStyles();
-    const [isCriminal, setIsCriminal] = useState(false);
+    const [isCriminal, setIsCriminal] = useState(true);
     const [loading, setLoading] = useState(false);
+    const [criminality, setCriminality] = useState<string>('');
 
     const handleImageUpload = async (e:any) => {
         setLoading(true);
@@ -41,9 +69,12 @@ export default function SignInPage(){
         let img = new Image();
         img.src = url;
         const predict = await getImageLabel(img);
+        setCriminality((predict[0].prob  * 100).toFixed(2).toString())
         if (predict[0].prob > 0.6) {
-            alert('Sorry your are too high risk ' + predict[0].prob + ' chance to be a criminal');
             setIsCriminal(true);
+        }
+        else {
+            setIsCriminal(false);
         }
         setLoading(false);
     };
@@ -55,7 +86,10 @@ export default function SignInPage(){
             </Header>
             <Card2>
                 <ThemeProvider theme={theme}>
-                    {loading ? <CircularProgress /> : <>
+                    <div className={classes.container}>
+                        <Typography variant='h4' className={classes.title}>
+                            Sign in
+                        </Typography>
                         <form className="form">
                             <InputBox required={true} type="text" autocomplete="username">
                                 Username
@@ -66,9 +100,34 @@ export default function SignInPage(){
                             <input
                                 accept="image/*"
                                 id="upload-file-button"
+                                style={{ display: 'none' }}
                                 type="file"
+                                multiple={false}
                                 onChange={handleImageUpload}
                             />
+                            <label htmlFor="upload-file-button">
+                                <div className={classes.wrapper}>
+                                    <Button
+                                        component="span"
+                                        variant="contained"
+                                        color="secondary"
+                                        disableElevation
+                                        fullWidth
+                                        className={classes.signButton}
+                                        startIcon={<CloudUploadIcon />}
+                                        disabled={loading}
+                                    >
+                                        Verify
+                                    </Button>
+                                    {loading && <CircularProgress size={24} className={classes.buttonProgress} />}
+                                </div>
+                            </label>
+                            <p className="text" style={isCriminal ? {color: 'red'} : {color: 'green'}}>
+                                {criminality ? `Likelyhood of being a criminal: ${criminality}%`
+                                    :
+                                    ''
+                                }
+                            </p>
                             <div>
                                 <Button type="submit" variant="contained" color="primary" disableElevation fullWidth className={classes.signButton} disabled={isCriminal}>
                                     Sign In
@@ -77,8 +136,8 @@ export default function SignInPage(){
                         </form>
                         <p className="text">
                             Don't have an account? <a href="https://www.w3schools.com/tags/tag_br.asp">Sign Up</a>
-                        </p></>
-                    }
+                        </p>
+                    </div>
                 </ThemeProvider>
             </Card2>
         </>
